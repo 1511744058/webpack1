72d95f1015199e4bb2c81a3ee0e8b230f3f2aa97
cyj <1511744058@qq.com>
2022-10-25 16:26:43 +0800

    css,less,scss,配置完成2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
-
 import App from './App'
-
+import './sass/public.scss'
 ReactDOM.render(
     <App/>,
     document.getElementById("app")
--- src/one.js
+++ src/one.js
@@ -9,3 +9,11 @@ function testable(target) {
 @testable //调用装饰器函数
 class MyTestableClass {}
 console.log("装饰器语法",MyTestableClass.isTestable) // true
+
+const delay=new Promise(resolve=>console.log("new Promise()"));
+function* helloWorldGenerator() {
+    yield 'hello';
+   yield 'world';
+   return 'ending';
+}
+var hw = helloWorldGenerator();
